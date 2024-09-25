0a658c91c53234ddd3c2030c2bd2488a2df1d7a4
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-09-26 00:08:03 +0530

    Add Navbar component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -56,10 +56,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Customer from './pages/Customer';
+import Navbar from './pages/Navbar';
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
---
+++ client/src/pages/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../Styles/Navbar.css';
+import logo from '../assets/2.png';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Font Awesome import
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img
+          src={logo}
+          alt="Nexi Logo"
+          className="logo"
+        />
+        <h2>NEXI</h2>
+      </div>
+      <div className="navbar-center">
+        <ul className="nav-links">
+          <li>
+            <a href="/login">Dashboard</a>
+          </li>
+          <li>
+            <a href="/login">Reports</a>
+          </li>
+          <li>
+            <a href="/login">About Us</a>
+          </li>
+          <li className='icon-li'>
+            <a href="/login" className="cart-icon">
+                <i className="fas fa-shopping-cart"></i>
+                <span className="cart-count">0</span> {/* Dynamic cart count can be updated here */}
+            </a>
+          </li>
+          <li className='icon-li'>
+            <a href="/login" className="user-icon">
+                <i className="fas fa-user"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
