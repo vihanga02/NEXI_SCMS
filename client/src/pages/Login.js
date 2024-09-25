7282ef93ac6b4cae0b33df3c5cfb53e16f0801e6
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-09-25 12:35:22 +0530

    Add Register.css, robots.txt, index.css, and package.json
    This commit adds the following files:
    - Register.css
    - robots.txt
    - index.css
    - package.json
    
    These files are necessary for the project and will be used for styling, defining website behavior, and managing dependencies.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,28 +1,33 @@
 import React, { useState } from 'react';
 import '../Styles/Login.css';
 import { Link } from 'react-router-dom';
+import logo from '../assets/2.png';
 
 const Login = () => {
-
+  
   const[email, setEmail] = useState('');
   const[password, setPassword] = useState('');
+
   function handleSubmit(event){
     event.preventDefault();
+    console.log(email, password);
   }
+  
 
   return (
     <div className="login-container">
       <div className="login-left">
-      <p className='text'>Welcome back! Please login to your account!!</p>
+      
         <div className="logo-container">
           <img
-            src="../../2.png" // Replace with your actual logo image path
+            src={logo} // Replace with your actual logo image path
             alt="Nexi Logo"
             className="logo"
           />
           <h2>NEXI</h2>
           <p>The Nexus of Distribution</p>
         </div>
+        <p className='text'>Welcome back! Please login to your account.</p>
         <form className="login-form" onSubmit={handleSubmit}>
           <input type="email" placeholder="Enter your Email Address" className="input-field" 
           onChange={e => setEmail(e.target.value)}/>
@@ -33,11 +38,7 @@ const Login = () => {
         <span>Don't you have an account?<Link to ='/register'>Register</Link></span>
       </div>
       <div className="login-right">
-        
           <h3>Streamlining your distribution from railway to doorstep with seamless efficiency.</h3>
-          
-        
-        
       </div>
     </div>
   );
