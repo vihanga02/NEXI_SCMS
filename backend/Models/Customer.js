213898840de5177ddea6dd8c990b6f394554e74d
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-10-08 13:44:38 +0530

    xZ

--- backend/Models/Customer.js
+++ backend/Models/Customer.js
@@ -1,15 +1,18 @@
-import pool from '../config/db.js';
+import pool from '../dbConfig.js';
 
 module.export = class Customer {
     static async getProduct() {
         const query = 'select * from Products';
-        pool.query(query,
-            (error, results, fields) => {
-                if (error) {
-                    reject(error);
-                };
-                resolve(results);
+        return new Promise((resolve, reject) => {
+            pool.query(query, (error, results, fields) => {
+            if (error) {
+                return reject(error);
             }
-        )
+            resolve(results);
+            });
+        });
     }
+
+    static async createOrder(request) {
+}
 }
