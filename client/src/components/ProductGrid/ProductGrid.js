8eda4841f901756c443856d48a44c41810f43fe1
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-10-15 11:44:33 +0530

    Update product price display and background styling

--- client/src/components/ProductGrid/ProductGrid.js
+++ client/src/components/ProductGrid/ProductGrid.js
@@ -10,7 +10,7 @@ const ProductGrid = ({ products }) => {
                 <Link key={product.Product_ID} to={`/products/category/${product.Product_ID}`} className="product-item">
                     <img src={product.Image_Link} alt={product.Product_Name} />
                     <div className="product-name">{product.Product_Name}</div>
-                    <div className="product-price">${product.Price}</div>
+                    <div className="product-price">LKR {product.Price}</div>
                     <div><button className="add-to-cart">Add To Cart</button></div>
                 </Link>
             ))}
