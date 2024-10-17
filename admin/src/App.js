e294b10c26108fd0cbf3b0419bd1e33c094eea90
heshanmadv <157682942+heshanmadv@users.noreply.github.com>
2024-10-17 10:26:25 +0530

    Admin
    admin template

---
+++ admin/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login/Login.js';
+import Signup from './pages/Signup/Signup.js';
+
+import Admindashboard from './pages/Admindashboard/Admindashboard.js';
+import Driver from './pages/Driver/Driver.js';
+import Assistant from './pages/Assistant/Assistant.js';
+import Report from './pages/Report/Report.js';
+import Order from './pages/Order/Order.js';
+import Delivery_schedule from './pages/Delivery_schedule/Delivery_schedule.js';
+const App = () => {
+  return (
+    <>
+      
+      <Routes>
+        
+        
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path='/admindashboard' element={<Admindashboard/>}/>
+        <Route path='/driver' element={<Driver/>}/>
+        <Route path='/assistant' element={<Assistant/>}/>
+        <Route path='/order' element={<Order/>}/>
+        <Route path='/delivery_schedule' element={<Delivery_schedule/>}/>
+        <Route path='/report' element={<Report/>}/>
+
+
+
+        
+       
+
+        
+
+
+      </Routes>
+     
+    
+    </>
+  );
+};
+
+// Wrap the App component with Router outside
+const WrappedApp = () => (
+<Router>
+  <App />
+</Router>
+);
+
+export default WrappedApp;
---
+++ admin/src/pages/Delivery_schedule/Delivery_schedule.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from '../../components/Sidebar/Sidebar.js'
+import Topbar from '../../components/Topbar/Topbar.js';
+import './Delivery_schedule.css'
+
+function delivery_schedule() {
+  return (
+    <div className='DScontainer'>
+      <Sidebar />
+      <div className='dscontainer'>
+        <Topbar/>
+      </div>
+    </div>
+  )
+}
+
+export default delivery_schedule
