460bd4343e17978b89c27cddc9f1e5b78e003707
heshanmadv <157682942+heshanmadv@users.noreply.github.com>
2024-10-29 11:16:50 +0530

    remove assistant

--- backend/Models/ManagerModel.js
+++ backend/Models/ManagerModel.js
@@ -479,6 +479,17 @@ class Manager {
       throw error;
     }
   }
+
+
+  static async removeAssistant(Assistant_ID) {
+    const query = "DELETE FROM Driver_assistant WHERE Assistant_ID = ?";
+    try {
+      const [results] = await pool.query(query, [Assistant_ID]);
+      return results;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 
