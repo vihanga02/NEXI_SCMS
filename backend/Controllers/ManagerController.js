a1e04b496c43e08bb787c4789ab1977c8be2b675
SSasindu <166973374+SSasindu@users.noreply.github.com>
2024-10-29 16:47:44 +0530

    error resolve

--- backend/Controllers/ManagerController.js
+++ backend/Controllers/ManagerController.js
@@ -507,16 +507,6 @@ async function getAvailabilityCounts(req, res) {
 }
 
 async function getIncompleteOrdersForStore(req, res) {
-<<<<<<< Updated upstream
-
-    const storeID = req.user.store;
-    try {
-        const result = await Manager.IncompletedTrainOrders(storeID);
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(500).json({ message: "Error fetching incomplete orders", error: error.message });
-    }
-=======
   const storeID = req.user.store;
   try {
     const result = await Manager.IncompletedTrainOrders(storeID);
@@ -527,7 +517,6 @@ async function getIncompleteOrdersForStore(req, res) {
       error: error.message,
     });
   }
->>>>>>> Stashed changes
 }
 
 async function insertDriver(req, res){
