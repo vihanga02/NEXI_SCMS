0c958cc3ffb2963d2579c1c5c878f3904cc6b624
SSasindu <166973374+SSasindu@users.noreply.github.com>
2024-10-30 17:41:12 +0530

    Remove trackedOrders state and streamline delivery scheduling error handling

--- admin/src/pages/Order/Order.js
+++ admin/src/pages/Order/Order.js
@@ -17,7 +17,6 @@ function Order() {
   const [selectedForTrain, setSelectedForTrain] = useState([]);
   const [compOrders, setCompOrders] = useState([]);
   const [paidOrders, setPaidOrders] = useState([]);
-  const [trackedOrders, setTrackedOrders] = useState([]);
   const [receivedOrders, setReceivedOrders] = useState([]);
   
   useEffect(() => {
@@ -95,8 +94,7 @@ function Order() {
 
   const trackingToTruck = async () => {
     try {
-      const response = await axios.post('/manager/queueForDelivery', { orderList:selectedForTruck, delID:delivery_id }, { withCredentials: true });
-      setTrackedOrders(response.data);
+      await axios.post('/manager/queueForDelivery', { orderList:selectedForTruck, delID:delivery_id }, { withCredentials: true });
 
       toast.success("Navigating to truck schedules!", {
         position: "top-right",
@@ -116,15 +114,24 @@ function Order() {
       }, 1000);
 
     } catch (error) {
-      console.error('Error getting orders', error.response ? error.response : error);
+      toast.error("Please create a delivery schedule!", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      navigate(`/delivery_schedule/`);
     }
   };
 
   const trackingToTrain = async () => {
 
     try {
-      const response = await axios.post('/manager/queueForDelivery', { orderList:selectedForTrain, delID:delivery_id }, { withCredentials: true });
-      setTrackedOrders(response.data);
+      await axios.post('/manager/queueForDelivery', { orderList:selectedForTrain, delID:delivery_id }, { withCredentials: true });
 
       toast.success("Navigating to truck schedules!", {
         position: "top-right",
@@ -144,7 +151,20 @@ function Order() {
       }, 1000);
 
     } catch (error) {
-      console.error('Error getting orders', error.response ? error.response : error);
+      toast.error("Please create a delivery schedule!", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      setTimeout(() => {
+        navigate(`/delivery_schedule/`);
+      }, 1500);
+      console.error('Error sending request to backend:', error);
     }
   };
 
